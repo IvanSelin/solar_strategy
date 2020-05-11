3aaf3461e698500557d65522e6d8f84d81dc1aa1
Ivan Selin <ivanselin93@gmail.com>
2020-05-11 19:38:27 +0300

    sin -> sind on the way to the solar power calculation

--- solar_radiation.jl
+++ solar_radiation.jl
@@ -1,21 +1,22 @@
+function calculate_noon_time()
+    days = [1:365;]
+    b = 360 * (days .- 81) / 365
+    t = 7.53 * cosd.(b) + 1.5 * sind.(b) - 9.87 * sind.(2b)
+    return t
+end
 
-
-function solar_power(latitude, time)
+function solar_power(latitude, time_df)
     transmittance=0.75 # transmittance (unitless)
     solar_constant=1367 # solar constant (w/m^2)
     p=101325 # normal atmospheric pressure
 
-    sun_maximum_inclination = 23.45
+    sun_maximum_declination = 23.45 # degrees
 
-    # TODO: calculate day from time
-    # time as an input parameter, not day
-
-
-
-    hours = collect(0:23) # or hours = [0:23;]
-    angle_by_hour = (12 .- hours) * 15 * pi / 180.0
-
-    decline_angle = 23.45 * sin(2 * pi * (284 + day) / 365) * pi / 180
+    # # some obsolete lines
+    # hours = collect(0:23) # or hours = [0:23;]
+    # angle_by_hour = (12 .- hours) * 15 * pi / 180.0
+    #
+    # decline_angle = 23.45 * sin(2 * pi * (284 + day) / 365) * pi / 180
 
     # Global Horizontal Irradiance (GHI) = Direct Normal Irradiace (DNI, beam) +
     # + Diffuse Horizontal Irradiance (DHI) * cos(z)
@@ -26,18 +27,45 @@ function solar_power(latitude, time)
 
     # https://brstu.ru/static/unit/journal_smt/docs/number-36/115-121.pdf
     # 360/365 - rotation of sun per day in degrees
-    s_0 = solar_constant * (1 + 0.033 * cosd(360 / 365 * day))
+    # solar radiation intensity
+    s_0 = solar_constant * (1 .+ 0.033 * cosd.(360 / 365 * time_df.year_day))
     # fixed formula? more sun during summer, day shift by 10
     # s_0 = solar_constant * (1 - 0.033 * cosd(360 / 365 * (day + 10))
     # or should there be cos instead of cosd?
 
-    # sun inclination angle
-    sun_inclination_angle = sun_maximum_inclination *
-        * sind.(360 * ( (284 .+ day) / 365) )
+    # sun declination  angle
+    sun_declination_angle = (sun_maximum_declination *
+        sind.(360 * ( (285 .+ time_df.year_day) / 365.25) )
+        )
+
+    # sun hour angle (ω)
+    # sun_hour_angle = ( 15 *
+
+    # sunrise/sunset angle
+    sunrise_angle = acosd.(- tand.(latitude) .* tand.(sun_declination_angle) )
+
+    # sun hours in a day
+    sun_hours = 2 * sunrise_angle / 15
+
+    # sun altitude angle (α)
+    # need hour angle!
+    altitude_angle = ( ( cosd.(latitude) .* cosd.(sun_declination_angle) .*
+        cosd.(hour_angle) ) .+ sind.(latitude) .* sind.(sun_declination_angle)
+        )
 
 
 
-    sin_a = sind.(latitude) * sind.()
+
+
+    w_s = acosd(-tand(59.93)*tand(sun_decl_spb))
+    sun_hours_test = (
+        2 * w_s / 15
+    )
+
+    sin_alpha = ( sind.(latitude) .* sind.(sun_declination_angle) .+
+        cosd.(latitude) .* cosd.(sun_declination_angle) .*
+        cosd.()
+        )
 
     # sun angle (6)
     # plot(sind.(360*(284 .+[1:365;])/365))
